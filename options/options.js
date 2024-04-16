0534f26555bae32ccb215038c3fd0f995a29cec6
Crowdbotics <team@crowdbotics.com>
2024-04-16 16:19:32 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "freseniusmedicalcar-47536",
+  url: "https://freseniusmedicalcar-47536.botics.co",
+  api: "https://freseniusmedicalcar-47536.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
